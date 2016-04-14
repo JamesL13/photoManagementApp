4749509edabb67b23b99d474b8f786316ace3e4c
Garrett Knox <garrettknox@Garretts-MacBook-Pro-2.local>
2016-04-14 00:41:22 -0500

    Testing Core Data

--- photoManagementAppB/NewProjectViewController.swift
+++ photoManagementAppB/NewProjectViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class NewProjectViewController: UIViewController {
 
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var projectNameField: UITextField!
+    @IBOutlet weak var projectKeywordField: UITextField!
+    @IBOutlet weak var projectDescriptionField: UITextView!
+    
+    var projectList = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let borderColor = UIColor(red:204.0/255.0, green:204.0/255.0, blue:204.0/255.0, alpha:1.0)
-        textView.layer.borderColor = borderColor.CGColor
-        textView.layer.borderWidth = 0.5
-        textView.layer.cornerRadius = 5.0
-
+        projectDescriptionField.layer.borderColor = borderColor.CGColor
+        projectDescriptionField.layer.borderWidth = 0.5
+        projectDescriptionField.layer.cornerRadius = 5.0
+        projectNameField.text = projectList[0].valueForKey("projectName")
         // Do any additional setup after loading the view.
     }
 
@@ -27,7 +32,26 @@ class NewProjectViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func loadProjectList()
+    {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName:"Project")
+        
+        do {
+            let fetchedResults = try managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
+            
+            if let results = fetchedResults {
+                projectList = results
+            }
+            else {
+                print("Could not fetch array")
+            }
+        } catch {
+            return
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
