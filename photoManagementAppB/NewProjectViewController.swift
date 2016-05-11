434e4b28b4845e241d84ae0e77231ce2a7b8b296
Garrett Knox <garrettknox@mwc-091003.dhcp.missouri.edu>
2016-05-11 14:28:23 -0500

    Fixed photo rotation issue
    PNG photos have an orientation flag that is most often set to right
    angle. JPEGs ignore setting this flag so using a JPEG file type
    prevents the image from rotating

--- photoManagementAppB/NewProjectViewController.swift
+++ photoManagementAppB/NewProjectViewController.swift
@@ -304,7 +304,8 @@ class NewProjectViewController: UIViewController, UICollectionViewDelegate, UICo
             let newPhotoEntity = NSEntityDescription.entityForName("Photo", inManagedObjectContext: managedContext)
             newPhoto = NSManagedObject(entity: newPhotoEntity!, insertIntoManagedObjectContext: managedContext)
         
-        let imageData: NSData! = UIImagePNGRepresentation(pickedImage)
+        //let imageData: NSData! = UIImagePNGRepresentation(pickedImage)
+        let imageData: NSData! = UIImageJPEGRepresentation(pickedImage, 1.0)
         
         newPhoto?.setValue(imageData, forKey: "photo")
         newPhoto?.setValue(self.newProject, forKey: "project")
